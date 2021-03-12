48c51166c5ab1ee9fc7820051547839b2514ce14
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-03-12 16:38:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -65,7 +65,7 @@ class TicTacToe
   end
 
   def won?
-    WIN_COMBINATIONS.each do  {|single_win|
+    WIN_COMBINATIONS.each do |single_win|
       win_index_1 = single_win[0]
       win_index_2 = single_win[1]
       win_index_3 = single_win[2]
@@ -74,11 +74,10 @@ class TicTacToe
       position_2 = @board[win_index_2]
       position_3 = @board[win_index_3]
    
-      if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+      if position_1 == position_2 && position_2 == position_3 && position_taken?(in)
         single_win
       end
     end
-    }
       false
   end
 
