50e27bdce18f18361eb3579f460ff56089ac39db
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-03-12 13:57:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,7 +52,7 @@ class TicTacToe
   end 
 
 
-  def turn
+  def turn(board)
     puts "Please enter 1-9:"
     input = gets.strip
     index = input_to_index(input)
@@ -82,7 +82,7 @@ class TicTacToe
       return false
   end
 
-  def full?
+  def full?(board)
     if @board.any? {|index| index == nil || index == " "}
       return false
     else
