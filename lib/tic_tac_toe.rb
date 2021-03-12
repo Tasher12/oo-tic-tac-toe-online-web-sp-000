45390148fe7f7db28e1eccb12d878b9262bc268b
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-03-12 15:30:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -59,7 +59,7 @@ class TicTacToe
     if valid_move?(index)
       move(index, current_player)
       display_board
-    else !valid_move(index)
+    else !valid_move?(index)
       turn 
     end
   end
