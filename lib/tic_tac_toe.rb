40deeb140a5f752873724cb8fa8cdfcedb92fb28
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-03-12 15:14:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -74,7 +74,7 @@ class TicTacToe
       position_3 = @board[win_index_3]
    
       if position_1 == position_2 && position_2 == position_3 && position_taken?(@board, win_index_1)
-        single_win.
+        single_win
       else 
         false
       end
