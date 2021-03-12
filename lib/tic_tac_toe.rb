3b0e24207ad38be759bd143f7b0976ecf9113bac
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-03-12 14:07:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,8 +57,7 @@ class TicTacToe
     input = gets.strip.to_i 
     index = input_to_index(input)
     if valid_move?(input)
-      player = current_player
-      move(index, player)
+      move(index, current_player)
       display_board
    
     end
