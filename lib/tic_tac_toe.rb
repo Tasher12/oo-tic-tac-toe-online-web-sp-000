99e698a9cd43aa8d702835ebf3a600d89497e1cb
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-03-12 14:13:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,8 +39,8 @@ class TicTacToe
     !(@board[index].nil? || @board[index] == " ")
   end 
   
-  def valid_move?(index)
-    !position_taken?(index) && index.between?(0,8)
+  def valid_move?
+    !position_taken? && index.between?(0,8)
   end
 
   def turn_count
